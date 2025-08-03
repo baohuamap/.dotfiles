00338e767158f99cbea1670b5b1c90a68cea05d4
Bao Hua <bhua@dnanexus.com>
2025-08-03 17:23:26 +0700

    update blur conf

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -65,7 +65,7 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+config.macos_window_background_blur = 50
 
 -- and finally, return the configuration to wezterm
 return config
