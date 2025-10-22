ea12a55264b417c58dff4aa68c40217e00f872c9
baohuamap <baohua3110@gmail.com>
2025-10-22 22:17:23 +0700

    add borders

--- sketchybar/.config/sketchybar/colors.lua
+++ sketchybar/.config/sketchybar/colors.lua
@@ -1,25 +1,25 @@
 return {
-  black = 0xff181819,
-  white = 0xffe2e2e3,
-  red = 0xfffc5d7c,
-  green = 0xff9ed072,
-  blue = 0xff76cce0,
-  yellow = 0xffe7c664,
-  orange = 0xfff39660,
-  magenta = 0xffb39df3,
-  grey = 0xff7f8490,
+  black = 0xff11111b,
+  white = 0xffcdd6f4,
+  red = 0xfff38ba8,
+  green = 0xffa6e3a1,
+  blue = 0xff89b4fa,
+  yellow = 0xfff9e2af,
+  orange = 0xfffab387,
+  magenta = 0xfff5c2e7,
+  grey = 0xff585b70,
   transparent = 0x00000000,
 
   bar = {
-    bg = 0xf02c2e34,
-    border = 0xff2c2e34,
+    bg = 0xff1e1e2e,
+    border = 0xff313244,
   },
   popup = {
-    bg = 0xc02c2e34,
-    border = 0xff7f8490
+    bg = 0xff181825,
+    border = 0xff45475a
   },
-  bg1 = 0xff363944,
-  bg2 = 0xff414550,
+  bg1 = 0xff313244,
+  bg2 = 0xff45475a,
 
   with_alpha = function(color, alpha)
     if alpha > 1.0 or alpha < 0.0 then return color end
